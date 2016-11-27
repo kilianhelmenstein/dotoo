cf13b3285a54ea22b17319f5e0b7dd4e23a0631d
Kilian <admin@example.com>
2016-11-28 00:14:50 +0100

    Added datalayer interface and sql impl. for modeltype 'task'

---
+++ dotoo/src/data/interface/datalyr_task.h
@@ -0,0 +1,52 @@
+#ifndef DATALYR_TASK_H
+#define DATALYR_TASK_H
+
+#include <QList>
+
+#include "project/libsymbolsexport.h"
+#include "data/interface/datalyr_types.h"
+#include "models/interface/types/commontypes.h"
+
+
+
+namespace Dotoo {
+
+class Task;
+
+
+namespace Data {
+
+
+/*!
+ * \brief   The TaskDataLyr class defines the layer for accessing
+ *          the data of model type 'Task'.
+ */
+class LIB_EXPORT DataLyr_Task
+{
+public:
+    /*!
+     * \brief   Contains all possible errors that could occur while using
+     *          this layer.
+     */
+    typedef enum {
+        NotFound,
+        NotAvailable,
+        InvalidArguments
+    } Error_t;
+
+public:
+    virtual ~DataLyr_Task() {}
+
+    virtual QList<Task> getAllTasks() throw(Error_t) = 0;
+    virtual Task getTask( UniqueId taskId ) throw(Error_t) = 0;
+    virtual void createTask( const Task& newTask ) throw(Error_t) = 0;
+    virtual void changeTask( const Task& changedTask ) throw(Error_t) = 0;
+    virtual void deleteTask( const UniqueId taskId ) throw(Error_t) = 0;
+};
+
+
+} // namespace Data
+
+} // namespace Dotoo
+
+#endif // DATALYR_TASK_H
