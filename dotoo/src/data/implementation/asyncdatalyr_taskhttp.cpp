97bd04e3b60d60ee7e015da7b3f4a1c9ce596469
Kilian <admin@example.com>
2016-12-09 21:51:35 +0100

    implemented error handling

--- dotoo/src/data/implementation/asyncdatalyr_taskhttp.cpp
+++ dotoo/src/data/implementation/asyncdatalyr_taskhttp.cpp
@@ -3,9 +3,9 @@
 #include <QNetworkReply>
 
 #include "asyncdatalyr_taskhttp.h"
+#include "data/utils/http.h"
 #include "models/interface/task.h"
 #include "modelparser/interface/bytestreamtaskparser.h"
-
 
 
 using namespace Dotoo;
@@ -16,7 +16,7 @@ using namespace Dotoo::ModelParser;
 
 AsyncDataLyr_TaskHttp::AsyncDataLyr_TaskHttp( const QString& url,
                                               const QString& resourceCollection,
-                                              const ModelParser::BytestreamTaskParser& parser)
+                                              BytestreamTaskParser* parser)
     : m_url( url ),
       m_resourceCollection( resourceCollection ),
       m_parser( parser ),
@@ -42,15 +42,28 @@ void AsyncDataLyr_TaskHttp::getAllTasks()
 
     auto handleResponse = [&]()
     {
-        QList<Task>* allTasks = new QList<Task>( m_parser.BytestreamIntoTaskList( reply->readAll() ) );
-        emit responseGetAllTasks( allTasks );
+        QList<Task>* allTasks = nullptr;
+
+        const HttpUtilz::HttpStatusCode statusCode
+                = static_cast<HttpUtilz::HttpStatusCode>(reply->attribute( QNetworkRequest::HttpStatusCodeAttribute ).toInt());
+        Error_t errorCode = HttpUtilz::DataErrorIntoHttpCode( statusCode );
+        if ( errorCode == Data::None )
+        {
+            try {
+                allTasks = new QList<Task>( m_parser->BytestreamIntoTaskList( reply->readAll() ) );
+            } catch (...) {
+                errorCode = Data::InvalidFormat;
+            }
+        }
+
+        emit responseGetAllTasks( allTasks, errorCode );
     };
 
     connect( reply, &QNetworkReply::readyRead, handleResponse );
 }
 
 
-void AsyncDataLyr_TaskHttp::getTask( UniqueId taskId )
+void AsyncDataLyr_TaskHttp::getTask( const UniqueId taskId )
 {
     QNetworkRequest request;
     request.setUrl( QString("%1/%2/%3").arg( m_url,
@@ -61,8 +74,21 @@ void AsyncDataLyr_TaskHttp::getTask( UniqueId taskId )
 
     auto handleResponse = [&]()
     {
-        Task* t = new Task( m_parser.BytestreamIntoTask( reply->readAll() ) );
-        emit responseGetTask( t );
+        Task* t = nullptr;
+
+        const HttpUtilz::HttpStatusCode statusCode
+                = static_cast<HttpUtilz::HttpStatusCode>(reply->attribute( QNetworkRequest::HttpStatusCodeAttribute ).toInt());
+        Error_t errorCode = HttpUtilz::DataErrorIntoHttpCode( statusCode );
+        if ( errorCode == Data::None )
+        {
+            try {
+                t = new Task( m_parser->BytestreamIntoTask( reply->readAll() ) );
+            } catch (...) {
+                errorCode = Data::InvalidFormat;
+            }
+        }
+
+        emit responseGetTask( t, errorCode );
     };
 
     connect( reply, &QNetworkReply::finished, handleResponse );
@@ -76,11 +102,13 @@ void AsyncDataLyr_TaskHttp::createTask( const Task& newTask )
                                           m_resourceCollection ) );
 
     QNetworkReply* reply = m_networkAccess->post( request,
-                                                  m_parser.TaskIntoBytestream( newTask )  );
+                                                  m_parser->TaskIntoBytestream( newTask )  );
 
-    auto handleResponse = [&]()
+    auto handleResponse = [this, reply]()
     {
-        emit responseCreateTask( None );
+        const HttpUtilz::HttpStatusCode statusCode
+                = static_cast<HttpUtilz::HttpStatusCode>(reply->attribute( QNetworkRequest::HttpStatusCodeAttribute ).toInt());
+        emit responseCreateTask( HttpUtilz::DataErrorIntoHttpCode( statusCode ) );
     };
 
     connect( reply, &QNetworkReply::finished, handleResponse );
@@ -95,11 +123,13 @@ void AsyncDataLyr_TaskHttp::changeTask( const Task& changedTask )
                                              QString::number( changedTask.getId() ) ) );
 
     QNetworkReply* reply = m_networkAccess->put( request,
-                                                 m_parser.TaskIntoBytestream( changedTask ) );
+                                                 m_parser->TaskIntoBytestream( changedTask ) );
 
-    auto handleResponse = [&]()
+    auto handleResponse = [this, reply]()
     {
-        emit responseCreateTask( None );
+        const HttpUtilz::HttpStatusCode statusCode
+                = static_cast<HttpUtilz::HttpStatusCode>(reply->attribute( QNetworkRequest::HttpStatusCodeAttribute ).toInt());
+        emit responseCreateTask( HttpUtilz::DataErrorIntoHttpCode( statusCode ) );
     };
 
     connect( reply, &QNetworkReply::finished, handleResponse );
@@ -115,9 +145,11 @@ void AsyncDataLyr_TaskHttp::deleteTask( const UniqueId taskId )
 
     QNetworkReply* reply = m_networkAccess->deleteResource( request );
 
-    auto handleResponse = [&]()
+    auto handleResponse = [this, reply]()
     {
-        emit responseCreateTask( None );
+        const HttpUtilz::HttpStatusCode statusCode
+                = static_cast<HttpUtilz::HttpStatusCode>(reply->attribute( QNetworkRequest::HttpStatusCodeAttribute ).toInt());
+        emit responseCreateTask( HttpUtilz::DataErrorIntoHttpCode( statusCode ) );
     };
 
     connect( reply, &QNetworkReply::finished, handleResponse );
