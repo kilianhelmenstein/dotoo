37e682865fb248e8ae79322c512b2ff5aad1a477
Kilian <admin@example.com>
2016-12-25 14:58:21 +0100

    Added own class for tasklist views

--- dotoo/clientapp/main.cpp
+++ dotoo/clientapp/main.cpp
@@ -1,9 +1,7 @@
 #include "mainwindow.h"
 #include <QApplication>
-#include <QPushButton>
-#include <QDate>
 #include <QVBoxLayout>
-#include <QScrollArea>
+#include <QPalette>
 
 #include "data/implementation/asyncdatalyr_taskhttp.h"
 #include "modelparser/taskjson.h"
@@ -29,22 +27,25 @@ int main(int argc, char *argv[])
     QWidget mainW;
 
     /* Dependencies used by MVC's: */
-    BytestreamTaskParser* parser = new JsonTaskParser();
     AsyncDataLyr_Task* dataLayer = new AsyncDataLyr_TaskHttp( "http://localhost:8080",
                                                               "tasks",
-                                                              parser );
-
+                                                              new JsonTaskParser() );
     TaskListViewModel* model = new TaskListViewModel(dataLayer);
-
-    QPalette defaultPalette;
-    TaskListView* view = new TaskListView(defaultPalette);
-    view->setModel(model);
-
     model->getAllTasks();
 
-    QVBoxLayout layout;
-    layout.addWidget(view);
-    mainW.setLayout(&layout);
+    QPalette* appPalette = new QPalette();
+    appPalette->setColor( QPalette::All, QPalette::Background, QColor("white") );
+    TaskListView* view = new TaskListView( QLatin1String("All Tasks"),
+                                           *appPalette );
+    view->setModel(model);
+
+    QVBoxLayout* layout = new QVBoxLayout();
+    layout->setSizeConstraint(QLayout::SetMinAndMaxSize);
+    layout->addWidget(view);
+
+    mainW.setSizePolicy( QSizePolicy::Expanding, QSizePolicy::Expanding );
+    mainW.setLayout(layout);
     mainW.show();
+
     return a.exec();
 }
