f70931449674c6cf5e8669fa274eb7330fffc624
Kilian <admin@example.com>
2017-01-04 20:07:46 +0100

    added translations

--- dotoo/clientapp/lang/dotoo_de.ts
+++ dotoo/clientapp/lang/dotoo_de.ts
@@ -4,27 +4,27 @@
 <context>
     <name>Dotoo::GUI::PersonEditView</name>
     <message>
-        <location filename="../views/appspec/personeditview.cpp" line="170"/>
+        <location filename="../views/appspec/personeditview.cpp" line="171"/>
         <source>Forename</source>
         <translation>Vorname</translation>
     </message>
     <message>
-        <location filename="../views/appspec/personeditview.cpp" line="171"/>
+        <location filename="../views/appspec/personeditview.cpp" line="172"/>
         <source>Name</source>
         <translation>Name</translation>
     </message>
     <message>
-        <location filename="../views/appspec/personeditview.cpp" line="172"/>
+        <location filename="../views/appspec/personeditview.cpp" line="173"/>
         <source>Comment</source>
         <translation>Kommentar</translation>
     </message>
     <message>
-        <location filename="../views/appspec/personeditview.cpp" line="173"/>
+        <location filename="../views/appspec/personeditview.cpp" line="174"/>
         <source>Abort</source>
         <translation>Abbrechen</translation>
     </message>
     <message>
-        <location filename="../views/appspec/personeditview.cpp" line="174"/>
+        <location filename="../views/appspec/personeditview.cpp" line="175"/>
         <source>Apply</source>
         <translation>Übernehmen</translation>
     </message>
@@ -32,40 +32,63 @@
 <context>
     <name>Dotoo::GUI::PersonListView</name>
     <message>
-        <location filename="../views/appspec/personlistview.cpp" line="67"/>
-        <location filename="../views/appspec/personlistview.cpp" line="231"/>
+        <location filename="../views/appspec/personlistview.cpp" line="68"/>
+        <location filename="../views/appspec/personlistview.cpp" line="232"/>
         <source>Search:</source>
         <translation>Suche:</translation>
     </message>
     <message>
-        <location filename="../views/appspec/personlistview.cpp" line="230"/>
+        <location filename="../views/appspec/personlistview.cpp" line="231"/>
         <source>All Persons</source>
         <translation>Alle Personen</translation>
     </message>
 </context>
 <context>
+    <name>Dotoo::GUI::SettingsView</name>
+    <message>
+        <location filename="../views/appspec/settingsview.cpp" line="146"/>
+        <source>Settings</source>
+        <translation>Einstellungen</translation>
+    </message>
+    <message>
+        <location filename="../views/appspec/settingsview.cpp" line="147"/>
+        <source>Language</source>
+        <translation>Sprache</translation>
+    </message>
+    <message>
+        <location filename="../views/appspec/settingsview.cpp" line="149"/>
+        <source>Abort</source>
+        <translation>Abbrechen</translation>
+    </message>
+    <message>
+        <location filename="../views/appspec/settingsview.cpp" line="150"/>
+        <source>Apply</source>
+        <translation>Abbrechen</translation>
+    </message>
+</context>
+<context>
     <name>Dotoo::GUI::TaskEditView</name>
     <message>
-        <source>Title</source>
-        <translation type="vanished">Titel</translation>
-    </message>
-    <message>
-        <location filename="../views/appspec/taskeditview.cpp" line="213"/>
-        <source>Forename</source>
-        <translation>Vorname</translation>
-    </message>
-    <message>
         <location filename="../views/appspec/taskeditview.cpp" line="214"/>
+        <source>Title</source>
+        <translation>Titel</translation>
+    </message>
+    <message>
+        <source>Forename</source>
+        <translation type="vanished">Vorname</translation>
+    </message>
+    <message>
+        <location filename="../views/appspec/taskeditview.cpp" line="215"/>
         <source>Comment</source>
         <translation>Kommentar</translation>
     </message>
     <message>
-        <location filename="../views/appspec/taskeditview.cpp" line="215"/>
+        <location filename="../views/appspec/taskeditview.cpp" line="216"/>
         <source>Abort</source>
         <translation>Abbruch</translation>
     </message>
     <message>
-        <location filename="../views/appspec/taskeditview.cpp" line="216"/>
+        <location filename="../views/appspec/taskeditview.cpp" line="217"/>
         <source>Apply</source>
         <translation>Übernehmen</translation>
     </message>
@@ -73,33 +96,33 @@
 <context>
     <name>Dotoo::GUI::TaskListView</name>
     <message>
-        <location filename="../views/appspec/tasklistview.cpp" line="254"/>
+        <location filename="../views/appspec/tasklistview.cpp" line="255"/>
         <source>All Tasks</source>
         <translation>Alle Aufgaben</translation>
     </message>
     <message>
-        <location filename="../views/appspec/tasklistview.cpp" line="255"/>
         <location filename="../views/appspec/tasklistview.cpp" line="256"/>
+        <location filename="../views/appspec/tasklistview.cpp" line="257"/>
         <source>Enable filter:</source>
         <translation>Filter aktiv:</translation>
     </message>
     <message>
-        <location filename="../views/appspec/tasklistview.cpp" line="257"/>
+        <location filename="../views/appspec/tasklistview.cpp" line="258"/>
         <source>Done:</source>
         <translation>Erledigt:</translation>
     </message>
     <message>
-        <location filename="../views/appspec/tasklistview.cpp" line="258"/>
+        <location filename="../views/appspec/tasklistview.cpp" line="259"/>
         <source>Yes</source>
         <translation>Ja</translation>
     </message>
     <message>
-        <location filename="../views/appspec/tasklistview.cpp" line="259"/>
+        <location filename="../views/appspec/tasklistview.cpp" line="260"/>
         <source>No</source>
         <translation>Nein</translation>
     </message>
     <message>
-        <location filename="../views/appspec/tasklistview.cpp" line="260"/>
+        <location filename="../views/appspec/tasklistview.cpp" line="261"/>
         <source>Search:</source>
         <translation>Suche:</translation>
     </message>
