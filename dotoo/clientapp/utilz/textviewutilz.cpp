fc3f4d0ca65f115426e3f7c992deb63526cff7e9
Kilian <admin@example.com>
2017-01-04 20:10:16 +0100

    don't add '...' if word wrapping is enabled

--- dotoo/clientapp/utilz/textviewutilz.cpp
+++ dotoo/clientapp/utilz/textviewutilz.cpp
@@ -12,10 +12,16 @@ namespace TextViewUtilz {
 void SetTextToLabel( QLabel *label,
                      const QString& text )
 {
-    QFontMetrics metrix(label->font());
-    int width = label->width() - 2;
-    QString clippedText = metrix.elidedText(text, Qt::ElideRight, width);
-    label->setText(clippedText);
+    if ( !label->wordWrap() )
+    {
+        QFontMetrics metrix(label->font());
+        int width = label->width() - 2;
+        QString clippedText = metrix.elidedText(text, Qt::ElideRight, width);
+        label->setText(clippedText);
+    } else
+    {
+        label->setText(text);
+    }
 }
 
 
